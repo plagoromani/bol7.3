e4594c5281193abb5acc3dc4157efe2ae28c6f05
Pablite5 <Pablite5@GAMING>
2015-11-10 10:17:59 +0100

    finalizado16

---
+++ src/bol7/pkg3/Bol73.java
@@ -0,0 +1,18 @@
+
+package bol7.pkg3;
+
+/**
+ *
+ * @author Pablite5
+ */
+public class Bol73 {
+
+    
+    public static void main(String[] args) {
+        int num;
+        Comparación obx= new Comparación();
+        num = obx.pedirNumero();
+        obx.comparaNum(num);
+    }
+    
+}
